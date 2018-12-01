a856d0bdc63c89706c09f2523121a08325def0d5
Ben Botvinick <benbotvinick@gmail.com>
2018-12-01 16:44:42 -0500

    add cocoapods and finish half of ui

---
+++ ios/Intentions/Intentions MessagesExtension/Models/Sentiment.swift
@@ -0,0 +1,25 @@
+//
+//  Sentiment.swift
+//  Intentions MessagesExtension
+//
+//  Created by Ben Botvinick on 12/1/18.
+//  Copyright © 2018 Ben Botvinick. All rights reserved.
+//
+
+import Foundation
+
+class Sentiment {
+    static let neutral = "😐"
+    static let worry = "😟"
+    static let happiness = "🙃"
+    static let sadness = "😢"
+    static let love = "🥰"
+    static let surprise = "😮"
+    static let fun = "😛"
+    static let relief = "😌"
+    static let hate = "😠"
+    static let empty = "😶"
+    static let enthusiasm = "🤗"
+    static let boredom = "😒"
+    static let anger = "😡"
+}
