de44633b2bc4b5774392743b393cb4a05a33391d
Ben Botvinick <benbotvinick@gmail.com>
2018-12-01 21:21:17 -0500

    ui almost done

---
+++ ios/Intentions/Intentions MessagesExtension/Extensions/UIViewController+Keyboard.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Keyboard.swift
+//  Intentions MessagesExtension
+//
+//  Created by Ben Botvinick on 12/1/18.
+//  Copyright © 2018 Ben Botvinick. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
