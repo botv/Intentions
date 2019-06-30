4080da8af3404499828cbad7f6b2f2ceabdd4815
ankner <zackankner@gmail.com>
2019-06-29 22:04:01 -0400

    model mvp

--- ml/models/pre_process.py
+++ ml/models/pre_process.py
@@ -1,23 +1,26 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
+import os
 import pandas as pd
 import numpy as np
 import string
 import nltk
+import pickle
 from nltk.corpus import stopwords
-from gensim.models import Word2Vec
+#from gensim.models import Word2Vec
 import re
 
 class Pre_Process(object):
 
 	def __init__(
-			self,training_threads,text_dir,chunk_size,
-			processed_input_dir, processed_label_dir
-		):
-		self.training_threads = training_threads
-		self.text_dir = text_dir
-		self.chunk_size = chunk_size
+			self, processed_input_dir, processed_label_dir,
+			BUFFER_SIZE, BATCH_SIZE, TAKE_SIZE
+			):
 		self.processed_input_dir = processed_input_dir
 		self.processed_label_dir = processed_label_dir
+		self.BUFFER_SIZE = BUFFER_SIZE
+		self.BATCH_SIZE = BATCH_SIZE
+		self.TAKE_SIZE = TAKE_SIZE
 
 
 	def clean_text(self):
@@ -30,56 +33,73 @@ class Pre_Process(object):
 					str.maketrans('', '', string.punctuation)
 					)
 				processed_text = processed_text.strip()
-				label = label
+				processed_text = nltk.word_tokenize(processed_text)
+				processed_text = [word for word in processed_text
+				 if word not in stopwords.words('english')]
+				processed_text = ' '.join([str(w) for w in processed_text])
 				with open(self.processed_input_dir, 'a') as pid:
 					pid.write(processed_text + '\n')
 				with open(self.processed_label_dir, 'a') as pld:
 					pld.write(label + '\n')
 
+	
+	def build_dataset(self):
+		content_dataset = tf.data.TextLineDataset(self.processed_input_dir)
+		sentiment_dataset = tf.data.TextLineDataset(self.processed_label_dir)
 
-	def retrieve_vocab(self):
-		unique_sentiments = []
-		unique_content = []
-		one_hot_ref = {}
-		with open(self.processed_label_dir, 'r') as pld:
-			for label in pld:
-				l = label[0:-1]
-				if l not in unique_sentiments:
-					unique_sentiments.append(l)
-		with open(self.processed_input_dir, 'r') as pid:
-			for sentence in pid:
-				s = nltk.word_tokenize(sentence)
-				for i in range(len(s)):
-					s[i] = [w for w in s[i] if w not in stopwords.words('english')]
-				for w in s:
-					if w not in unique_content:
-						unique_content.append(w)
-		for i, us in enumerate(unique_sentiments):
-			one_hot_sent[us] = i
-		for i, uc in enumerate(unique_content):
-			one_hot_cont[uc] = i
-		return one_hot_cont, one_hot_sent
+		data = tf.data.Dataset.zip((content_dataset,sentiment_dataset))
+		data = data.shuffle(
+				self.BUFFER_SIZE,
+				reshuffle_each_iteration=False
+				)
 
-	def save_as_array(self,one_hot_sentiment):
-		complete_content = []
-		complete_labels = []
-		with open(self.processed_input_dir,'r') as pid:
-			for sentence in pid:
-				s = nltk.word_tokenize(sentence)
-				for i in range(len(s)):
-					s[i] = [w for w in s[i] if w not in stopwords.words('english')]
-				complete_content.append(s)
-		with open(self.processed_label_dir,'r') as pld:
-			for label in pld:
-				complete_labels.append(one_hot_sentiment[label[0:-1]])
+		tokenizer = tfds.features.text.Tokenizer()
 
+		content_vocab = set()
+		sentiment_vocab = set()
+		for content_tensor, sentiment_tensor in data:
+			content_tokens = tokenizer.tokenize(content_tensor.numpy())
+			sentiment_tokens = tokenizer.tokenize(sentiment_tensor.numpy())
+			content_vocab.update(content_tokens)
+			sentiment_vocab.update(sentiment_tokens)
 
+		vocab_size = len(content_vocab) + 1
+		label_size = len(sentiment_vocab)
 
+		sample_one_hot = np.zeros(label_size)		
+		
+		content_encoder = tfds.features.text.TokenTextEncoder(content_vocab)
+		sentiment_encoder = tfds.features.text.TokenTextEncoder(sentiment_vocab)
+
+		def encode(content_tensor,label_tensor):
+			encoded_content = content_encoder.encode(content_tensor.numpy())
+			encoded_label = sentiment_encoder.encode(label_tensor.numpy())[0]
+			one_hot_label = sample_one_hot
+			one_hot_label[encoded_label - 1] = 1
+			return encoded_content, one_hot_label
+
+		def encode_map_fn(text, label):
+  			return tf.py_function(encode, inp=[text, label], Tout=(tf.int64, tf.int64))
+
+		data = data.map(encode_map_fn)
+
+		train_data = data.skip(self.TAKE_SIZE).shuffle(self.BUFFER_SIZE)
+		train_data = train_data.padded_batch(self.BATCH_SIZE, padded_shapes=([-1],[-1]))
+
+		test_data = data.take(self.TAKE_SIZE)
+		test_data = test_data.padded_batch(self.BATCH_SIZE, padded_shapes=([-1],[-1]))
+
+		return train_data, test_data, vocab_size, label_size
+'''
 if __name__ == '__main__':
 	p = Pre_Process(
 		2,'../data/text_emotion.csv',5000,
-		'../data/content.txt', '../data/label.txt'
+		'../data/content.txt', '../data/label.txt',
+		'../data/content_dict.p', '../data/sentiment_dict.p',
+		40000, 10000, 5000
 	)
-	p.clean_text()
-	one_hot_content,one_hot_sentiment = p.retrieve_sentiment_vocab()
-	p.save_as_array(one_hot_sentiment)
+	#p.clean_text()
+	#p.build_dataset()
+	#content_dict,sentiment_dict = p.retrieve_vocab()
+	#p.enumerate(content_dict,sentiment_dict)
+'''
---
+++ ml/models/sentiment_analysis_model.py
@@ -0,0 +1,113 @@
+import tensorflow as tf 
+from tensorflow.keras.layers import (
+	Dense,
+	LSTM,
+	Embedding,
+	Bidirectional
+	)
+from tensorflow.keras import Model
+import numpy as np
+
+import pre_process
+
+tf.keras.backend.clear_session()
+
+p = pre_process.Pre_Process('../data/content.txt', '../data/label.txt',40000, 10000, 5000)
+
+train_ds, test_ds, VOCAB_SIZE, NUM_LABELS = p.build_dataset()
+
+EMBEDDING_DIM = 100
+#NUM_FATURES =
+SAVE_PATH = '../saved_models/sentiment_analysis_model'
+EPOCHS = 500
+LSTM_CELLS_1 = 256
+LSTM_CELLS_2 = 128
+DENSE_UNITS_1 = 64
+DENSE_UNITS_2 = 32
+
+class sentiment_analysis_lstm(Model):
+	def __init__(self):
+		super(sentiment_analysis_lstm, self).__init__()
+		self.embedding_1 = Embedding(VOCAB_SIZE,EMBEDDING_DIM)
+		self.lstm_1 = Bidirectional(LSTM(LSTM_CELLS_1, return_sequences=True))
+		self.lstm_2 = Bidirectional(LSTM(LSTM_CELLS_2))
+		self.dense_1 = Dense(DENSE_UNITS_1, activation='relu')
+		self.dense_2 = Dense(DENSE_UNITS_2, activation='relu')
+		self.dense_3 = Dense(NUM_LABELS, activation='softmax')
+	def call(self,x):
+		x = self.embedding_1(x)
+		x = self.lstm_1(x)
+		x = self.lstm_2(x)
+		x = self.dense_1(x)
+		x = self.dense_2(x)
+		x = self.dense_3(x)
+		return x
+
+
+
+model = sentiment_analysis_lstm()
+
+
+
+loss_object = tf.keras.losses.CategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
+
+
+
+@tf.function
+def train_step(sentences, labels):
+	with tf.GradientTape() as tape:
+		predictions = model(sentences)
+		loss = loss_object(labels, predictions)
+		gradients = tape.gradient(loss, model.trainable_variables)
+		optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+	train_loss(loss)
+	train_accuracy(labels, predictions)
+
+
+@tf.function
+def test_step(images, labels):
+	predictions = model(images)
+	t_loss = loss_object(labels, predictions)
+
+	test_loss(t_loss)
+	test_accuracy(labels, predictions)
+
+
+
+
+for epoch in range(EPOCHS):
+	for inputs,labels in train_ds:
+		train_step(inputs,labels)
+
+	for test_inputs,test_labels in test_ds:
+		test_step(test_inputs,test_labels)
+
+	template = 'Epoch: {}, Loss: {}, Acc: {}, Test Loss: {}, Test Acc: {}'
+
+	print(template.format(
+		epoch+1,train_loss.result(),train_accuracy.result()*100,test_loss.result(),
+		test_accuracy.result()*100)
+	)
+
+model.save_weights(SAVE_PATH,save_format='tf')
+
+
+
+
+
+
+
+
+
+
+
+
+
